7bca5a28a9831139286ff6a08ee2c60a27baf545
Mahmoud Reda <mahmodreda219@gmail.com>
2023-03-27 01:00:03 +0200

    refactor href

--- js/mainJS.js
+++ js/mainJS.js
@@ -1,29 +1,12 @@
-fetch('https://api.quran.com/api/v4/chapters')
+fetch('https://api.quran.com/api/v4/chapters?fields=text_uthmani')
   .then(response => response.json())
   .then(data => {
+    console.log(data)
     const chapters = data.chapters;
+    let list = document.querySelector(".main_list ul"); // Select the parent <ul> element
+
     for (let i = 0; i < chapters.length; i++) {
-      let list = document.querySelector(".main_list ul"); // Select the parent <ul> element
-      list.innerHTML +=  "<li><a href="+ chapters[i].id +" >" +chapters[i].name_arabic+ "</a></li>";
-  
-  
-     
-  }
-    
+      list.innerHTML += `<li><a href=surah.html?id=${chapters[i].id}>سورة ${chapters[i].name_arabic}</a></li>`;
+    }
   })
   .catch(error => console.error(error));
-
-  fetch("https://api.quran.com/api/v4/quran/verses/uthmani")
-  .then((response) => response.json())
-  .then((data) => {
-    // Get verses
-    const verses = data.verses;
-
-   
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
-
-
- 
