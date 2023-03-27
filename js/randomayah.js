7a871d121d69cd712f2c94657d995ed3cd731e69
Mahmoud Reda <mahmodreda219@gmail.com>
2023-03-28 01:47:57 +0200

    finish get random ayah

--- js/randomayah.js
+++ js/randomayah.js
@@ -1,24 +1,15 @@
-let randomNumber = Math.floor(Math.random() * 6234) + 1;
-
 let ayaTag = document.querySelector(".random_aya_h2");
 let surahName = document.getElementById("surah-name");
 let ayaNumber = document.getElementById("aya-number");
 
-// Send GET request using Fetch API
-fetch(`http://api.alquran.cloud/v1/ayah/${randomNumber}?fields=text_uthmani`)
+fetch(`https://api.quran.com/api/v4/verses/random?language=ar&words=true&fields=text_uthmani`)
   .then((response) => response.json())
   .then((data) => {
-    // Get random verse
-    const varese = data.data;
 
-    ayaTag.innerHTML = ` ${varese.text}`;
-    surahName.innerHTML = `${varese.surah.name}`;
-    ayaNumber.innerHTML = `الاية :${varese.numberInSurah}`;
+    fetch(`https://api.quran.com/api/v4/chapters/${data.verse.verse_key.split(':')[0]}?language=ar`)
+      .then(surahRes => surahRes.json())
+      .then(surah => surahName.innerHTML = surah.chapter.name_arabic)
 
-    console.log();
-    console.log(data);
-    // console.log(verse.)
+    ayaTag.innerHTML = ` ${data.verse.text_uthmani}`;
+    ayaNumber.innerHTML = `الاية :${data.verse.verse_number}`;
   })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
