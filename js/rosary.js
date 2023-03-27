ec0f74d2ab95d485b717e31b3af6e8eb2adaed9b
Mahm0udFarrag <116115229+Mahm0udFarrag@users.noreply.github.com>
2023-03-28 00:47:31 +0200

    new new sora

--- js/rosary.js
+++ js/rosary.js
@@ -1,20 +1,18 @@
 let rosary_counter = 0;
-
 rosary_counter = localStorage.getItem('old_rosary_count') ?? 0
-
 document.getElementById('rosary-btn').innerHTML = rosary_counter
 
 document.getElementById('rosary-btn').onclick = function () {
-  rosary_counter++;
-  if (rosary_counter % 33 == 0) {
+    rosary_counter++;
+    if (rosary_counter % 33 == 0) {
 
-    document.getElementById('33_click_changer').innerHTML = 'لا إله إلا الله';
-  }
-  document.getElementById('rosary-btn').innerHTML = rosary_counter;
-  localStorage.setItem('33_click_changer', rosary_counter)
+        document.getElementById('33_click_changer').innerHTML = 'لا إله إلا الله';
+    }
+    document.getElementById('rosary-btn').innerHTML = rosary_counter;
+    localStorage.setItem('33_click_changer', rosary_counter)
 
-  localStorage.setItem('old_rosary_count', rosary_counter)
+    localStorage.setItem('old_rosary_count', rosary_counter)
 
-  console.log(rosary_counter);
+    console.log(rosary_counter);
 };
 
