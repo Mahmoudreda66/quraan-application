9e187f530482537636a66f5e0b0d0885a45c8b61
Mahmoud Tariq <mhmtthenetherlord@gmail.com>
2023-03-27 22:15:53 +0200

    next and previous buttons

--- js/surah.js
+++ js/surah.js
@@ -10,3 +10,26 @@ let playSurahAudio = _ => {
         playIcon.classList.replace('fa-pause', 'fa-play');
     }
 }
+let url = new URLSearchParams(location.search)
+function nextSurah(){
+    let next_surah = document.getElementById('next_surah')
+    let id =Number(url.get('id'))
+    id+=1
+    if(id > 114){
+        next_surah.href = `surah.html?id=0`
+    }
+    else{
+        next_surah.href = `surah.html?id=${id}`
+    }
+}
+function prevSurah(){
+    let prev_surah = document.getElementById('prev_surah')
+    let id =Number(url.get('id'))
+    id -=1
+    if(id >= 0){
+        prev_surah.href = `surah.html?id=${id}`
+    }
+    else{
+        prev_surah = `surah.html?id=0`
+    }
+}
