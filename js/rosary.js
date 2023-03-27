7878c18319ee3d8b931421ee863b7711807ce6f8
Mahmoud Reda <mahmodreda219@gmail.com>
2023-03-28 00:29:10 +0200

    refactoring rosary

--- js/rosary.js
+++ js/rosary.js
@@ -1,30 +1,20 @@
-let rosary_counter = 0 ;
-let old_rosary_count = 0;
+let rosary_counter = 0;
 
-rosary_counter = localStorage.getItem('old_rosary_count')
+rosary_counter = localStorage.getItem('old_rosary_count') ?? 0
 
-if (rosary_counter==0) 
-    { 
-    document.getElementById('rosary-btn').innerHTML=0;
-    }
-    else 
-    {
-        document.getElementById('rosary-btn').innerHTML= rosary_counter;  
-    }
-document.getElementById('rosary-btn').onclick = function()
-{
-rosary_counter++;
-if (rosary_counter%33==0) {
-    
-    document.getElementById('33_click_changer').innerHTML='لا إله إلا الله';
+document.getElementById('rosary-btn').innerHTML = rosary_counter
 
-    
-}
-document.getElementById('rosary-btn').innerHTML=rosary_counter;
-localStorage.setItem('33_click_changer',rosary_counter)
+document.getElementById('rosary-btn').onclick = function () {
+  rosary_counter++;
+  if (rosary_counter % 33 == 0) {
 
-localStorage.setItem('old_rosary_count',rosary_counter)
+    document.getElementById('33_click_changer').innerHTML = 'لا إله إلا الله';
+  }
+  document.getElementById('rosary-btn').innerHTML = rosary_counter;
+  localStorage.setItem('33_click_changer', rosary_counter)
 
-console.log(rosary_counter);
+  localStorage.setItem('old_rosary_count', rosary_counter)
+
+  console.log(rosary_counter);
 };
 
