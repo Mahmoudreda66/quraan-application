5bf5a61c4d342ce5086a276a0483c7587069908b
Mahmoud Reda <mahmodreda219@gmail.com>
2023-03-27 23:02:59 +0200

    some fixes

--- js/surah.js
+++ js/surah.js
@@ -1,5 +1,6 @@
 let surahAudio = document.getElementById('surahAudio'),
-    playIcon = document.getElementById('play-icon');
+    playIcon = document.getElementById('play-icon'),
+    url = new URLSearchParams(location.search);
 
 let playSurahAudio = _ => {
     if (surahAudio.paused) {
@@ -10,32 +11,34 @@ let playSurahAudio = _ => {
         playIcon.classList.replace('fa-pause', 'fa-play');
     }
 }
-let url = new URLSearchParams(location.search)
-function nextSurah(){
+
+(function nextSurah() {
     let next_surah = document.getElementById('next_surah')
-    let id =Number(url.get('id'))
-    id+=1
-    if(id > 114){
+    let id = Number(url.get('id'))
+    id += 1
+    if (id > 114) {
         next_surah.href = `surah.html?id=1`
     }
-    else{
+    else {
         next_surah.href = `surah.html?id=${id}`
     }
-}
-function prevSurah(){
+})();
+
+(function prevSurah() {
     let prev_surah = document.getElementById('prev_surah')
-    let id =Number(url.get('id'))
-    id -=1
-    if(id > 0){
+    let id = Number(url.get('id'))
+    id -= 1
+    if (id > 0) {
         prev_surah.href = `surah.html?id=${id}`
     }
-    else{
-        prev_surah = `surah.html?id=1`
+    else {
+        prev_surah.href = `surah.html?id=1`
     }
-}
+})();
+
 const surah = fetch(`https://api.quran.com/api/v4/verses/by_chapter/${url.get('id')}?language=ar&per_page=all&fields=text_uthmani`).then(response => response.json()).then(data => {
     let surah_content = document.getElementsByClassName('surah-content mt-3 mb-0')[0]
-    for(let i = 0; i < data.verses.length - 1; i++){
+    for (let i = 0; i < data.verses.length - 1; i++) {
         surah_content.textContent += data.verses[i].text_uthmani + "۝"
     }
     surah_content.textContent += data.verses[data.verses.length - 1].text_uthmani
