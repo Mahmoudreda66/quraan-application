9c48f5da3a6cbb47ddf5bd9294ed2e8056c45fc2
Mahmoud Tariq <mhmtthenetherlord@gmail.com>
2023-03-27 22:48:43 +0200

    surah content

--- js/surah.js
+++ js/surah.js
@@ -33,6 +33,10 @@ function prevSurah(){
         prev_surah = `surah.html?id=1`
     }
 }
-const surah = fetch(`https://api.quran.com/api/v4/verses/by_chapter/${url.get('id')}?language=ar&words=true`).then(response => response.json()).then(data => {
-    console.log(data)
+const surah = fetch(`https://api.quran.com/api/v4/verses/by_chapter/${url.get('id')}?language=ar&per_page=all&fields=text_uthmani`).then(response => response.json()).then(data => {
+    let surah_content = document.getElementsByClassName('surah-content mt-3 mb-0')[0]
+    for(let i = 0; i < data.verses.length - 1; i++){
+        surah_content.textContent += data.verses[i].text_uthmani + "۝"
+    }
+    surah_content.textContent += data.verses[data.verses.length - 1].text_uthmani
 })
