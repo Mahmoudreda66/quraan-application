328ba4460303ddd1a5b58cd9a893d758d27840b7
Mahmoud Reda <mahmodreda219@gmail.com>
2023-04-16 02:11:16 +0200

    finish v1

--- js/randomayah.js
+++ js/randomayah.js
@@ -5,11 +5,13 @@ let ayaTag = document.querySelector(".random_aya_h2"),
 fetch(`https://api.quran.com/api/v4/verses/random?language=ar&words=true&fields=text_uthmani`)
   .then((response) => response.json())
   .then((data) => {
+    let surahId = data.verse.verse_key.split(':')[0];
 
-    fetch(`https://api.quran.com/api/v4/chapters/${data.verse.verse_key.split(':')[0]}?language=ar`)
+    fetch(`https://api.quran.com/api/v4/chapters/${surahId}?language=ar`)
       .then(surahRes => surahRes.json())
       .then(surah => surahName.innerHTML = surah.chapter.name_arabic)
 
     ayaTag.innerHTML = ` ${data.verse.text_uthmani}`;
     ayaNumber.innerHTML = `الاية :${data.verse.verse_number}`;
+    document.getElementById('random-aya-link').href = 'surah.html?id=' + surahId;
   })
