05fc7f11347fadd70c6c09eda25238021c468bc0
Mahmoud Reda <mahmodreda219@gmail.com>
2023-05-07 12:05:14 +0200

    finish prayers

--- js/prayers-times.js
+++ js/prayers-times.js
@@ -1,62 +1,42 @@
-// Check if user's location is already stored in local storage
+let today = new Date(),
+    dayLink = today.getDate(),
+    monthLink = today.getMonth() + 1,
+    yearLink = today.getFullYear(),
+    userLat = localStorage.getItem('userLatitude') ?? '30.0444',
+    userLon = localStorage.getItem('userLongitude') ?? '31.2357';
+
 if (!localStorage.getItem('userLatitude') || !localStorage.getItem('userLongitude')) {
-    // Get user's location using browser's geolocation API
     navigator.geolocation.getCurrentPosition(
         function (position) {
-            // Store user's location in local storage
             localStorage.setItem('userLatitude', position.coords.latitude);
             localStorage.setItem('userLongitude', position.coords.longitude);
+            location.reload();
         },
         function (error) {
-            console.error('Error getting user location:', error);
-            // Set default location to Cairo if user denies location access
             localStorage.setItem('userLatitude', '30.0444');
             localStorage.setItem('userLongitude', '31.2357');
         }
     );
 }
-// =============================================================
 
-// fetch data from API
-let today = new Date();
-let dayLink = today.getDate()
-let monthLink = today.getMonth() + 1;
-let yearLink = today.getFullYear()
-console.log(dayLink, monthLink, yearLink);
-console.log(today);
-
-let fajrTime = document.getElementById('elFajr')
-let sunriseTime = document.getElementById('elSunrise')
-let dhuhrTime = document.getElementById('elDhuhr')
-let asrTime = document.getElementById('elAsr')
-let maghribTime = document.getElementById('elMaghrib')
-let ishaTime = document.getElementById('elIsha')
-let weekDay = document.getElementById('weekDay')
-let gregDate = document.getElementById('gregDate')
-let hijriDate = document.getElementById('hijriDate')
-
-
-let getCalendar = () => {
-    let url = `https://api.aladhan.com/v1/calendar/${yearLink}/${monthLink}?latitude=${localStorage.getItem('userLatitude')}&longitude=${localStorage.getItem('userLongitude')}`
+let getCalendar = (lat, long) => {
+    let url = `https://api.aladhan.com/v1/calendar/${yearLink}/${monthLink}?latitude=${lat}&longitude=${long}`
     fetch(url)
         .then(response => response.json())
         .then(data => {
             const apiId = dayLink - 1;
-            weekDay.textContent = `${data.data[apiId].date.hijri.weekday.ar}`
-            gregDate.innerHTML = `${data.data[apiId].date.readable}`
-            hijriDate.innerHTML = `${data.data[apiId].date.hijri.date}`
+            document.getElementById('weekDay').textContent = `${data.data[apiId].date.hijri.weekday.ar}`
+            document.getElementById('gregDate').innerHTML = `${data.data[apiId].date.readable}`
+            document.getElementById('hijriDate').innerHTML = `${data.data[apiId].date.hijri.date}`
 
-            fajrTime.innerHTML = `${data.data[apiId].timings.Fajr.substring(0, 5)}`
-            sunriseTime.innerHTML = `${data.data[apiId].timings.Sunrise.substring(0, 5)}`
-            dhuhrTime.innerHTML = `${data.data[apiId].timings.Dhuhr.substring(0, 5)}`
-            asrTime.innerHTML = `${data.data[apiId].timings.Asr.substring(0, 5)}`
-            maghribTime.innerHTML = `${data.data[apiId].timings.Maghrib.substring(0, 5)}`
-            ishaTime.innerHTML = `${data.data[apiId].timings.Isha.substring(0, 5)}`
-            console.log(data.data[apiId]);
-
+            document.getElementById('elFajr').innerHTML = `${data.data[apiId].timings.Fajr.substring(0, 5)}`
+            document.getElementById('elSunrise').innerHTML = `${data.data[apiId].timings.Sunrise.substring(0, 5)}`
+            document.getElementById('elDhuhr').innerHTML = `${data.data[apiId].timings.Dhuhr.substring(0, 5)}`
+            document.getElementById('elAsr').innerHTML = `${data.data[apiId].timings.Asr.substring(0, 5)}`
+            document.getElementById('elMaghrib').innerHTML = `${data.data[apiId].timings.Maghrib.substring(0, 5)}`
+            document.getElementById('elIsha').innerHTML = `${data.data[apiId].timings.Isha.substring(0, 5)}`
         })
         .catch(error => console.error(error));
 }
 
-getCalendar()
-// http://api.aladhan.com/v1/calendar/2023/05?latitude=30.5765383&longitude=31.5040656
+getCalendar(userLat, userLon);
