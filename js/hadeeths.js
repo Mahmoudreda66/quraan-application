6fafbce67040ede7b70bedea3480233a18c2443f
mahmmoud walid mahmoud <modywmbadr@gmail.com>
2023-08-03 19:25:17 +0300

    Pagination in haedths_Page is good

--- js/hadeeths.js
+++ js/hadeeths.js
@@ -18,18 +18,29 @@ if (dataBooksRef === "ahmad") totalHadiths = 4305;
 if (dataBooksRef === "darimi") totalHadiths = 2949;
 if (dataBooksRef === "malik") totalHadiths = 1587;
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    location.reload()
+    location.reload();
     dataBooksRef = button.getAttribute("data-books");
     localStorage.setItem("dataBooksRef", dataBooksRef);
     getHadiths(currentPage);
   });
 });
 console.log(dataBooksRef);
-buttons.forEach(rawybutton => {
+buttons.forEach((rawybutton) => {
   if (dataBooksRef === rawybutton.getAttribute("data-books")) {
-    rawybutton.classList.add("border", "border-3", "border-primary", "border-bottom-3", "border-top-0", "border-end-0", "border-start-0", "rounded-0", "fs-4", "text-primary")
+    rawybutton.classList.add(
+      "border",
+      "border-3",
+      "border-primary",
+      "border-bottom-3",
+      "border-top-0",
+      "border-end-0",
+      "border-start-0",
+      "rounded-0",
+      "fs-4",
+      "text-primary"
+    );
   } else {
     rawybutton.style.backgroundColor = "";
   }
@@ -42,22 +53,42 @@ function updateActivePageButton() {
 
   pageButtons.forEach((button, index) => {
     if (index + 1 === currentPage) {
-      button.classList.add("border", "border-3", "text-primary", "border-primary", "border-top-0", "border-end-0", "border-start-0", "rounded-0", "fs-4")
+      button.classList.add(
+        "border",
+        "border-3",
+        "text-primary",
+        "border-primary",
+        "border-top-0",
+        "border-end-0",
+        "border-start-0",
+        "rounded-0",
+        "fs-4"
+      );
     } else {
-      button.classList.remove("border", "border-3", "text-primary", "border-primary", "border-top-0", "border-end-0", "border-start-0", "rounded-0", "fs-4")
+      button.classList.remove(
+        "border",
+        "border-3",
+        "text-primary",
+        "border-primary",
+        "border-top-0",
+        "border-end-0",
+        "border-start-0",
+        "rounded-0",
+        "fs-4"
+      );
     }
   });
 }
 
-let getHadiths = page => {
+let getHadiths = (page) => {
   currentPage = page;
   const startIndex = (page - 1) * hadithsPerPage + 1;
   const endIndex = startIndex + hadithsPerPage - 1;
   let url = `https://api.hadith.gading.dev/books/${dataBooksRef}?range=${startIndex}-${endIndex}`;
 
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       hadeeths.innerHTML = "";
 
       for (let i = 0; i < data.data.hadiths.length; i++) {
@@ -65,32 +96,41 @@ let getHadiths = page => {
         hadeethsDiv.innerHTML = `
           <span>${data.data.hadiths[i].arab}</span> <br><br>
         `;
-        hadeethsDiv.classList.add("hadeethsDiv", "pt-5", "border", "border-tertiary", "border-2", "border-top-0", "border-end-0", "border-start-0",)
+        hadeethsDiv.classList.add(
+          "hadeethsDiv",
+          "pt-5",
+          "border",
+          "border-tertiary",
+          "border-2",
+          "border-top-0",
+          "border-end-0",
+          "border-start-0"
+        );
         hadeeths.appendChild(hadeethsDiv);
       }
       console.log(totalHadiths);
       console.table(data);
       updateActivePageButton();
     })
-    .catch(error => console.log("error", error));
+    .catch((error) => console.log("error", error));
 };
 
 // -=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 const previousButton = document.createElement("button");
 previousButton.innerText = "السابق";
-previousButton.classList.add("btn", "border-dark")
+previousButton.classList.add("btn", "border-dark", "previousButton");
 previousButton.addEventListener("click", () => {
   if (currentPage > 1) {
     getHadiths(currentPage - 1);
   }
 });
-pagination.appendChild(previousButton);
+document.querySelector(".previousButton").appendChild(previousButton);
 
-for (let i = 1; i <= (Math.ceil(totalHadiths / hadithsPerPage)) - 1; i++) {
+for (let i = 1; i <= Math.ceil(totalHadiths / hadithsPerPage) - 1; i++) {
   const pageButton = document.createElement("button");
   pageButton.innerText = i;
-  pageButton.classList.add("btn")
+  pageButton.classList.add("btn");
   pageButton.addEventListener("click", () => {
     getHadiths(i);
   });
@@ -99,7 +139,7 @@ for (let i = 1; i <= (Math.ceil(totalHadiths / hadithsPerPage)) - 1; i++) {
 
 const nextButton = document.createElement("button");
 nextButton.innerText = "التالي";
-nextButton.classList.add("btn", "border-dark")
+nextButton.classList.add("btn", "border-dark", "nextButton");
 nextButton.addEventListener("click", () => {
   let lastPage = Math.ceil(totalHadiths / hadithsPerPage);
   console.log(lastPage);
@@ -107,7 +147,14 @@ nextButton.addEventListener("click", () => {
     getHadiths(currentPage + 1);
   }
 });
-pagination.appendChild(nextButton);
+document.querySelector(".nextButton").appendChild(nextButton);
+
+const hiddeneBtn = document.createElement("button");
+hiddeneBtn.classList.add("btn", "overflow-hidden", "w-25");
+hiddeneBtn.style.border = "none";
+hiddeneBtn.style.pointerEvents = "none";
+pagination.insertAdjacentElement("afterbegin", hiddeneBtn);
+const clonedBtn = hiddeneBtn.cloneNode(true);
+pagination.insertAdjacentElement("beforeend", clonedBtn);
 
 getHadiths(currentPage);
-
