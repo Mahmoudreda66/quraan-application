01a03d57d2ffab66912791d0d3ff583629de544d
mahmmoud walid mahmoud <modywmbadr@gmail.com>
2023-08-16 19:15:08 +0300

    add audio_surah_aagamy_not-completed

--- js/surah.js
+++ js/surah.js
@@ -24,12 +24,154 @@ let url = new URLSearchParams(location.search);
     }
 })();
 
-const surah = fetch(`https://api.quran.com/api/v4/verses/by_chapter/${url.get('id')}?language=ar&per_page=all&fields=text_uthmani`).then(response => response.json()).then(data => {
-    let surah_content = document.getElementsByClassName('surah-content mt-3 mb-0')[0]
-    for (let i = 0; i < data.verses.length - 1; i++) {
-        surah_content.textContent += data.verses[i].text_uthmani + `〘${i + 1}〙`
-    }
-    surah_content.textContent += data.verses[data.verses.length - 1].text_uthmani
-})
+const surah = fetch(
+  `https://api.quran.com/api/v4/verses/by_chapter/${url.get(
+    "id"
+  )}?language=ar&per_page=all&fields=text_uthmani`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    let surah_content = document.getElementsByClassName(
+      "surah-content mt-3 mb-0"
+    )[0];
+    for (let i = 0; i < data.verses.length; i++) {
+      const spanAya = document.createElement("span");
+      spanAya.classList.add(`aya_${i + 1}`);
+      const ayat = data.verses[i].text_uthmani;
+      spanAya.appendChild(document.createTextNode(ayat));
+      surah_content.appendChild(spanAya);
 
-localStorage.setItem("Progress", url.get('id'));
+      const verseNumber = document.createElement("span");
+      verseNumber.textContent = `〘${i + 1}〙`;
+      spanAya.appendChild(verseNumber);
+
+      // console.log(spanAya);
+    }
+
+    const lastVerse = document.createElement("span");
+    lastVerse.textContent = data.verses[data.verses.length - 1].text_uthmani;
+    surah_content.appendChild(lastVerse);
+  });
+
+localStorage.setItem("Progress", url.get("id"));
+
+// audio player =======================================================
+
+const idSurah = +url.get("id");
+console.log(idSurah);
+let recitation;
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://quran-endpoint.vercel.app/quran");
+
+    if (!response.ok) {
+      throw new Error("Network response was not work");
+    }
+
+    const data = await response.json();
+    for (ele of data.data) {
+      if (ele.number === idSurah) {
+        recitation = ele.recitation.full;
+        document.querySelector(".surahNameAudio").textContent =
+          ele.asma.ar.long;
+          console.log(ele);
+      }
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+fetchData();
+
+const audioPlayer = document.querySelector(".audio-player");
+const audio = new Audio();
+fetchData().then(() => {
+  // console.log(recitation);
+  audio.src = recitation;
+});
+
+audio.addEventListener(
+  "loadeddata",
+  () => {
+    audioPlayer.querySelector(".time .length").textContent = getTimeCodeFromNum(
+      audio.duration
+    );
+    audio.volume = 0.75;
+  },
+  false
+);
+
+const timeline = audioPlayer.querySelector(".timeline");
+timeline.addEventListener(
+  "click",
+  (e) => {
+    const timelineWidth = window.getComputedStyle(timeline).width;
+    const timeToSeek = (e.offsetX / parseInt(timelineWidth)) * audio.duration;
+    audio.currentTime = timeToSeek;
+  },
+  false
+);
+
+const volumeSlider = audioPlayer.querySelector(".controls .volume-slider");
+volumeSlider.addEventListener(
+  "click",
+  (e) => {
+    const sliderWidth = window.getComputedStyle(volumeSlider).width;
+    const newVolume = e.offsetX / parseInt(sliderWidth);
+    audio.volume = newVolume;
+    audioPlayer.querySelector(".controls .volume-percentage").style.width =
+      newVolume * 100 + "%";
+  },
+  false
+);
+
+setInterval(() => {
+  const progressBar = audioPlayer.querySelector(".progress");
+  progressBar.style.width = (audio.currentTime / audio.duration) * 100 + "%";
+  audioPlayer.querySelector(".time .current").textContent = getTimeCodeFromNum(
+    audio.currentTime
+  );
+}, 500);
+
+const playBtn = audioPlayer.querySelector(".controls .toggle-play");
+playBtn.addEventListener(
+  "click",
+  () => {
+    if (audio.paused) {
+      playBtn.classList.remove("play");
+      playBtn.classList.add("pause");
+      audio.play();
+    } else {
+      playBtn.classList.remove("pause");
+      playBtn.classList.add("play");
+      audio.pause();
+    }
+  },
+  false
+);
+
+audioPlayer.querySelector(".volume-button").addEventListener("click", () => {
+  const volumeEl = audioPlayer.querySelector(".volume-container .volume");
+  audio.muted = !audio.muted;
+  if (audio.muted) {
+    volumeEl.classList.remove("icono-volumeMedium");
+    volumeEl.classList.add("icono-volumeMute");
+  } else {
+    volumeEl.classList.add("icono-volumeMedium");
+    volumeEl.classList.remove("icono-volumeMute");
+  }
+});
+
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
+
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+  ).padStart(2, 0)}`;
+}
